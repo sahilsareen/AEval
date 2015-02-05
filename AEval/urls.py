6c1935839d3270d46cfd91f99d5f66158d3d2570
Sahil Sareen <sahil.sareen@hotmail.com>
2015-02-05 19:28:39 +0530

    A basic working app
    * Most basic operations implemented
    * Tables in place
    * No CSS yet

--- AEval/urls.py
+++ AEval/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'AEval.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^interview/', include('interview.urls', namespace="interview")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', include('sky_visitor.urls')),
 )
--- interview/views.py
+++ interview/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from interview.models import Question, Choice
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'interview/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'interview/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'interview/results.html'
+
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'interview/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'interview/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+'''
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'interview/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('interview:results', args=(p.id,)))
